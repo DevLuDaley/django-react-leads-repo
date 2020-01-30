2dc57f3742c8a1be645b199482a3510b867106a5
DevLuDaley <daleyluis@gmail.com>
2020-01-29 19:59:53 -0500

    enable 'frontend' app

--- django-drf-react-quickstart/project/project/settings.py
+++ django-drf-react-quickstart/project/project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leads',
+    'leads', # enable leads app
     'rest_framework',  # enable restframework
+    'frontend',  # enable frontend app
 ]
 
 MIDDLEWARE = [
